81b4103730b99a5e6a5709278599d92be779d3c0
DavidStevens1997 <david.hr97110105243@gmail.com>
2022-09-13 20:50:13 -0500

    tercera homework

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -18,7 +18,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === '#') return 'id';
+  else if(selector[0] === '.') return 'class';
+  else if(selector.split('.').length > 1) return 'tag.class';
+  else return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +33,26 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = function(el){
+    return `#${el.id}` === selector;
+   }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(el){
+      let classes = el.classList;
+      for (let i = 0; i < classes.length; i++) {
+        if('.' + classes[i] === selector) return true;
+      }
+      return false; 
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(el){
+      let [tagBuscado, classBuscada] = selector.split('.');
+      return matchFunctionMaker(tagBuscado)(el) && matchFunctionMaker(selector=`.${classBuscada}`)(el);
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(el){
+      return el.tagName.toLowerCase() === selector;
+    }
   }
   return matchFunction;
 };
