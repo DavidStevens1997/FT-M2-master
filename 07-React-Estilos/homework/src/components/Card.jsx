7a2736c0cf0323d9e771e310575ed774deca046b
David Hurtado <david.hr97110105243@gmail.com>
2022-09-27 13:16:26 -0500

    7hw

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return(
+    <>
+      <button onClick={onClose}>X</button>
+      <h3>{name}</h3>
+      <div>
+        <h3>Min</h3>
+        <p>{min}</p>
+      </div>
+      <div>
+        <h3>Max</h3>
+        <p>{max}</p>
+      </div>
+      <div><img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt={"img"}/></div>
+     
+      
+    </>
+  )
 };
