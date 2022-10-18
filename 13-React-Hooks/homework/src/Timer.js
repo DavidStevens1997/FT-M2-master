c846693c71a21d1466fa9fbdca832a81829cc2a8
David Hurtado <david.hr97110105243@gmail.com>
2022-10-18 15:55:32 -0500

    13hw

--- 13-React-Hooks/homework/src/Timer.js
+++ 13-React-Hooks/homework/src/Timer.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+
 const Timer = () => {
+
+  const [segundos, setSegundos] = React.useState(0);
+  const [activo, setActivo] = React.useState(false);
+  const [tipo, setTipo] = React.useState('Contador');
+
   return (
     <div className="app">
-      Componente Timer
+      <div className="time">
+        {segundos}
+      </div>
+      <div className="row">
+        <button className="button-primary">
+          Inicio
+        </button>
+        <button className="button-secondary">
+          Reset
+        </button>
+      </div>
+      <button className="button">
+          Contador
+      </button>
+      <input type="number" placeholder="Ingresa Segundos" autoComplete="off"/>
     </div>
   );
 };
